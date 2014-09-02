19bc525e5d6e0fd4573e18b4625ecfa42f8c6a1e
Eduardo Borio <eduborio@gmail.com>
2014-09-02 08:25:11 -0300

    Criando classe DAO.

--- src/br/com/weblogia/domain/dao/CreateDAO.java
+++ src/br/com/weblogia/domain/dao/CreateDAO.java
@@ -6,42 +6,151 @@ import java.util.List;
 
 public class CreateDAO {
     	
-	private List<String> linhas= new ArrayList<String>();
+	private List<String> linhasRepositorio= new ArrayList<String>();
+	private List<String> linhasDAO= new ArrayList<String>();
 	private Class<?> classe;
 	
-	public void criaInterfacerepositorio(String className) {
+	public CreateDAO(String className) {
 		ClassLoader cl = ClassLoader.getSystemClassLoader();
 		try {
 			classe = cl.loadClass(className);
-			
-			linhas.add("public interface "+classe.getSimpleName()+"Repositorio{\r\n");
-			linhas.add("    Long buscaPorId(Long id);\r\n");
-			linhas.add("    List<"+classe.getSimpleName()+"> buscaTodos(int pagina);\r\n");
-			linhas.add("    List<"+classe.getSimpleName()+"> buscaTodos();\r\n");
-			linhas.add("    Long buscaTotalDeRegistrosDaLista();\r\n");
-			linhas.add("    void salva("+classe.getSimpleName()+" "+ classe.getSimpleName().toLowerCase() +");\r\n");
-			linhas.add("    void atualiza("+classe.getSimpleName()+" "+ classe.getSimpleName().toLowerCase() +");\r\n");
-			
-			
-			for(Field field : classe.getDeclaredFields()) {
-				System.out.println(field.getName());
-				if(field.getName().equals("nome"))
-					linhas.add("    List<"+classe.getSimpleName()+"> busca"+classe.getSimpleName()+"PorNome(String nome);\r\n");
-				
-				if(field.getName().equals("descricao"))
-					linhas.add("    List<"+classe.getSimpleName()+"> busca"+classe.getSimpleName()+"PorDescricao(String descricao);\r\n");
-					
-			}
-			linhas.add("    void setEmpresa(Empresa empresa);\r\n");
-			linhas.add("}");
-			
-			System.out.println(classe.getPackage());
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void criaInterfaceRepositorio() {
+		
+		linhasRepositorio.add(montaPackage(classe.getPackage().toString())+"repositorios;\r\n");
+		linhasRepositorio.add("\r\n");
+		linhasRepositorio.add("import java.util.List;\r\n");
+		linhasRepositorio.add("import "+classe.getPackage().getName()+"."+classe.getSimpleName() +";\r\n");
+			
+		linhasRepositorio.add("public interface "+classe.getSimpleName()+"Repositorio{\r\n");
+		linhasRepositorio.add("    Long buscaPorId(Long id);\r\n");
+		linhasRepositorio.add("    List<"+classe.getSimpleName()+"> buscaTodos(int pagina);\r\n");
+		linhasRepositorio.add("    List<"+classe.getSimpleName()+"> buscaTodos();\r\n");
+		linhasRepositorio.add("    Long buscaTotalDeRegistrosDaLista();\r\n");
+		linhasRepositorio.add("    void salva("+classe.getSimpleName()+" "+ classe.getSimpleName().toLowerCase() +");\r\n");
+		linhasRepositorio.add("    void atualiza("+classe.getSimpleName()+" "+ classe.getSimpleName().toLowerCase() +");\r\n");
+			
+		for(Field field : classe.getDeclaredFields()) {
 
-	public List<String> getLinhas() {
-		return linhas;
+			if(field.getName().equals("nome"))
+				linhasRepositorio.add("    List<"+classe.getSimpleName()+"> busca"+classe.getSimpleName()+"PorNome(String nome);\r\n");
+			
+			if(field.getName().equals("descricao"))
+				linhasRepositorio.add("    List<"+classe.getSimpleName()+"> busca"+classe.getSimpleName()+"PorDescricao(String descricao);\r\n");
+					
+		}
+		linhasRepositorio.add("    void setEmpresa(Empresa empresa);\r\n");
+		linhasRepositorio.add("}\r\n");
 	}
+	
+	public void criaClasseDAO() {
+		
+		linhasDAO.add(montaPackage(classe.getPackage().toString())+"repositorios.daos;\r\n");
+		linhasDAO.add("\r\n");
+		linhasDAO.add("import java.util.List;\r\n");
+		linhasDAO.add("import "+classe.getPackage().getName()+"."+classe.getSimpleName() +";\r\n");
+		linhasDAO.add("import javax.enterprise.context.RequestScoped;\r\n");
+		linhasDAO.add("import javax.persistence.Query;\r\n");
+		linhasDAO.add("\r\n");
+		linhasDAO.add("@SuppressWarnings(\"unchecked\")\r\n");
+		linhasDAO.add("@RequestScoped\r\n");
+			
+		linhasDAO.add("public class "+classe.getSimpleName()+"DAO extends DAO<"+classe.getSimpleName()+"> implements "+ classe.getSimpleName()+"Repositorio{\r\n");
+		linhasDAO.add("\r\n");
+		linhasDAO.add("    public "+classe.getSimpleName()+"DAO()\r\n");
+		linhasDAO.add("        super("+classe.getSimpleName()+".class);\r\n");
+		linhasDAO.add("    }\r\n");
+		linhasDAO.add("\r\n");
+		
+		for(Field field : classe.getDeclaredFields()) {
+			
+			if(field.getName().equals("nome")) {
+				
+				String alias = classe.getSimpleName().toLowerCase();
+				
+				linhasDAO.add("    @Override\r\n");
+				linhasDAO.add("    public List<"+classe.getSimpleName()+"> busca"+classe.getSimpleName()+"PorNome(String nome) {\r\n");
+				linhasDAO.add("        StringBuilder hql = new StringBuilder()\r\n");
+				linhasDAO.add("        hql.append(\" From "+ classe.getSimpleName() +" "+alias+" \"); \r\n");
+				linhasDAO.add("        hql.append(\" where "+alias+ ".nome like :nome \"); \r\n");
+				linhasDAO.add("        hql.append(\" and "+alias+".empresa = :empresa \");\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("        hql.append(\" order by "+alias+".nome \");\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("        Query query = entityManager.createQuery(hql.toString());\r\n");
+				linhasDAO.add("        query.setParameter(\"nome\", \"%\" + nome + \"%\");\r\n");
+				linhasDAO.add("        query.setParameter(\"empresa\", this.empresa);\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("        return query.getResultList();\r\n");
+				linhasDAO.add("    }\r\n");
+				
+			}
+			
+			if(field.getName().equals("descricao")) {
+				String alias = classe.getSimpleName().toLowerCase();
+				
+				linhasDAO.add("    @Override\r\n");
+				linhasDAO.add("    public List<"+classe.getSimpleName()+"> busca"+classe.getSimpleName()+"PorDescricao(String descricao) {\r\n");
+				linhasDAO.add("        StringBuilder hql = new StringBuilder()\r\n");
+				linhasDAO.add("        hql.append(\" From "+ classe.getSimpleName() +" "+alias+" \"); \r\n");
+				linhasDAO.add("        hql.append(\" where "+alias+ ".descricao like :descricao \"); \r\n");
+				linhasDAO.add("        hql.append(\" and "+alias+".empresa = :empresa \");\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("        hql.append(\" order by "+alias+".descricao \");\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("        Query query = entityManager.createQuery(hql.toString());\r\n");
+				linhasDAO.add("        query.setParameter(\"descricao\", \"%\" + descricao + \"%\");\r\n");
+				linhasDAO.add("        query.setParameter(\"empresa\", this.empresa);\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("\r\n");
+				linhasDAO.add("        return query.getResultList();\r\n");
+				linhasDAO.add("    }\r\n");
+				
+			}
+				
+		}
+		linhasDAO.add("}\r\n");
+	}
+
+	public List<String> getLinhasRepositorio() {
+		return linhasRepositorio;
+	}
+	
+	public List<String> getLinhasDAO() {
+		return linhasDAO;
+	}
+
+	public Class<?> getClasse() {
+		return classe;
+	}
+	
+	private String montaPackage(String packageDaClasseDeDominio) {
+		
+		int pontos = 0;
+		int index = 0;
+		
+		for(char c : packageDaClasseDeDominio.toCharArray()) {
+			
+			if(pontos == 3)
+				break;
+			
+			if(c == '.') {
+				pontos++;
+			}
+			index++;
+		}
+		
+		return packageDaClasseDeDominio.substring(0, index);
+		
+	}
+	
 }
--- test/br/com/borio/modelos/TesteDAO.java
+++ test/br/com/borio/modelos/TesteDAO.java
@@ -6,13 +6,17 @@ public class TesteDAO {
 	
 	public static void main(String[] args) {
 		
-		CreateDAO createDAO = new CreateDAO();
-		createDAO.criaInterfacerepositorio("br.com.weblogia.domain.Produto");
+		CreateDAO createDAO = new CreateDAO("br.com.weblogia.domain.Produto");
+		createDAO.criaInterfaceRepositorio();
+		createDAO.criaClasseDAO();
 		
-		for(String linha: createDAO.getLinhas()) {
+		for(String linha: createDAO.getLinhasRepositorio()) {
 			System.out.print(linha);
 		}
 		
+		for(String linha: createDAO.getLinhasDAO()) {
+			System.out.print(linha);
+		}
 		
 	}
 
