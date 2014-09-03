09a92a55a1a2b33751cb113ca70e967f05a8a516
Eduardo Borio <eduborio@gmail.com>
2014-09-03 08:58:43 -0300

    Carregando classe a partir de arquivo.

---
+++ test/br/com/borio/modelos/TestURlClassLoader.java
@@ -0,0 +1,37 @@
+package br.com.borio.modelos;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class TestURlClassLoader {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		File file = new File("C:\\Borius\\vendas\\lite\\src\\main\\webapp\\WEB-INF\\classes");
+
+		try {
+		    // Convert File to a URL
+		    @SuppressWarnings("deprecation")
+			URL url = file.toURL();          // file:/c:/myclasses/
+		    URL[] urls = new URL[]{url};
+
+		    // Create a new class loader with the directory
+		    ClassLoader cl = new URLClassLoader(urls);
+
+		    // Load in the class; MyClass.class should be located in
+		    // the directory file:/c:/myclasses/com/mycompany
+		    Class<?> cls = cl.loadClass("br.com.weblogia.domain.Produto");
+		    for(Field field: cls.getDeclaredFields()) {
+		    	System.out.println(field.getName());
+		    }
+		    
+		    
+		} catch (MalformedURLException e) {
+		} catch (ClassNotFoundException e) {
+		}
+	}
+
+}
